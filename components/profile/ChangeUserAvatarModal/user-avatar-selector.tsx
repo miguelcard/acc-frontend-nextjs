36cd2edde1d270212e5266adc4b57cb5c9e2aac2
Miguel Angel Cardenas <migueluy360@hotmail.com>
2025-08-24 21:28:12 +0200

    changed robot avatar option name

--- components/profile/ChangeUserAvatarModal/user-avatar-selector.tsx
+++ components/profile/ChangeUserAvatarModal/user-avatar-selector.tsx
@@ -64,7 +64,7 @@ export default function UserAvatarSelector({ user }: UserAvatarSelectorProps) {
 
 
 const seedsExamplesList: string[] = [
-    "ZapCat",
+    "ZapCats",
     "FizzFox",
     "Walle",
     "Rooster",
