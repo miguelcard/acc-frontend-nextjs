3cb653cc9e05cae565b76b87b707f0f7d1205a5d
Miguel Angel Cardenas <migueluy360@hotmail.com>
2026-05-26 23:32:20 +0200

    fixed modal high from edit habit

--- components/space/ScoreCard/score-card-sizes/HabitOptionsMenu/habit-options-menu.tsx
+++ components/space/ScoreCard/score-card-sizes/HabitOptionsMenu/habit-options-menu.tsx
@@ -104,7 +104,7 @@ export const HabitOptionsMenu = ({ habit }: { habit: HabitT }) => {
                             }
                             childrenTitle={option.name}
                             childrenBody={option.childrenBody}
-                            keyboardLift={0.6}
+                            keyboardLift={1}
                         />
                     ) : (
                         <Box key={option.name}>
